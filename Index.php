c101d471a6cc31e5a13f005ca05e46c4795f1c1a
Emma-Jonna <johanna.emma.pihl@gmail.com>
2025-07-15 12:19:39 +0200

    restructure
    restructured the code and changed to have php files.

---
+++ Index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+        <a class="" type="button" href="diceRoller/index.html">Dice Roller</a>
+        <a class="" type="button" href="randomiser/index.php">Randomiser</a>
+        <a class="" type="button" href="characterSheet/index.html">Character Sheet</a>
+    </nav>
+    <main>
+        this is the home page
+    </main>
+    <footer></footer>
+</body>
+
+</html>
